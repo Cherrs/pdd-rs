c827f381a6336fbefbbde442814d2cd7b1e3ce15
KuriyamaMirai <lovef@outlook.com>
2023-09-07 14:47:56 +0800

    删除bench

--- benches/my_benchmark.rs
+++
@@ -1,9 +0,0 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-
-fn criterion_benchmark(c: &mut Criterion) {
-    // let runtime = tokio::runtime::Runtime::new().unwrap();
-    c.bench_function("build_sign", |b| b.iter(|| {}));
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
