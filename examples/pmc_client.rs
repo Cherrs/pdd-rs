efd9d560513b53c2c08ac767016b9ebc53b2f261
KuriyamaMirai <lovef@outlook.com>
2023-12-01 11:25:27 +0800

    新增断线重连examples

--- examples/pmc_client.rs
+++ examples/pmc_client.rs
@@ -1,9 +1,9 @@
 use futures_util::StreamExt;
 use pdd::{
-    pmc::{CommandType, PmcClient},
+    pmc::{PmcClient, PmcConsumers},
     Config,
 };
-use tracing::{trace, Level};
+use tracing::{error, Level};
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
@@ -12,15 +12,22 @@ async fn main() -> anyhow::Result<()> {
         .with_max_level(Level::TRACE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    loop {
+        let client = PmcClient::new(Config::from_env()?);
+        let mut s = client.connect().await?;
+        msg_handler(&mut s).await;
+        error!("连接断开,5秒后重新连接");
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    }
+}
 
-    let client = PmcClient::new(Config::from_env()?);
-    let mut s = client.connect().await?;
-    // let aaa = Arc::new(Mutex::new(s));
-
+async fn msg_handler(s: &mut PmcConsumers) {
     while let Some(msg) = s.next().await {
+        if msg.is_err() {
+            return;
+        }
         let msg = msg.unwrap();
         println!("{:?}", msg);
         s.ack(&msg).await;
     }
-    Ok(())
 }
